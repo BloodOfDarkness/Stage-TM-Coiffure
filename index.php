5b5a99cb700a41882ff48c3a6ec8c019c8ae4c49
Blood <berserkbloody024@gmail.com>
2026-06-08 12:32:35 +0200

    test

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/main.css">
+    <script src="src/bootstrap.bundle.min.js"></script>
+    <title>TM-Coiffure</title>
+    <?php
+    // Récupère tous les fichiers image du dossier img/
+    $images = glob('img/*.{jpg,jpeg,png,webp,gif}', GLOB_BRACE);
+    ?>
+
+<body class="bg-khakibeige" id="top">
+    <?php include 'views/header.php'; ?>
+    <?php include 'views/hero.php'; ?>
+    <?php include 'views/prestation.php'; ?>
+    <?php include 'views/aPropos.php'; ?>
+    <?php include 'views/footer.php'; ?>
+</body>
+</html>
