2da82abb613032f3ed6a49fdb731b0229652c189
Blood <berserkbloody024@gmail.com>
2026-06-08 14:41:28 +0200

    Fix script source path in index.php

--- index.php
+++ index.php
@@ -5,12 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/main.css">
-    <script src="src/bootstrap.bundle.min.js"></script>
+    <script src="css/bootstrap.bundle.min.js"></script>
     <title>TM-Coiffure</title>
     <?php
     // Récupère tous les fichiers image du dossier img/
     $images = glob('img/*.{jpg,jpeg,png,webp,gif}', GLOB_BRACE);
     ?>
+</head>
 
 <body class="bg-khakibeige" id="top">
     <?php include 'views/header.php'; ?>
